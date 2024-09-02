c525e4945a0466575fc83ae9e59f6626771fa4c0
frarlo8 <farmentalo8@gmail.com>
2024-09-02 16:43:36 +0200

    first commit

---
+++ MotorsportDrivers.WPF/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MotorsportDrivers.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Motorsport Drivers Project
