5777771f1be0ebc5a09b1109f3c3b8050a8c0bea
frarlo8 <farmentalo8@gmail.com>
2024-09-16 12:05:37 +0200

    feat: Implemented Add/Edit modal ViewModels.

---
+++ MotorsportDrivers.WPF/ViewModels/AddMotorsportDriverViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorsportDrivers.WPF.ViewModels
+{
+    public class AddMotorsportDriverViewModel : ViewModelBase
+    {
+
+        public MotorsportDriverDetailsFormViewModel MotorsportDriverDetailsFormViewModel { get; }
+
+        public AddMotorsportDriverViewModel()
+        {
+            MotorsportDriverDetailsFormViewModel = new MotorsportDriverDetailsFormViewModel();
+        }
+
+    }
+}
---
+++ MotorsportDrivers.WPF/ViewModels/MotorsportDriverDetailsFormViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MotorsportDrivers.WPF.ViewModels
+{
+    public class MotorsportDriverDetailsFormViewModel : ViewModelBase
+    {
+
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        private bool _isWorldChampion;
+
+        public bool IsWorldChampion
+        {
+            get
+            {
+                return _isWorldChampion;
+            }
+            set
+            {
+                _isWorldChampion = value;
+                OnPropertyChanged(nameof(IsWorldChampion));
+            }
+        }
+
+        private string _country;
+
+        public string Country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                _country = value;
+                OnPropertyChanged(nameof(Country));
+            }
+        }
+
+        public bool CanSubmit => !string.IsNullOrEmpty(Name);
+
+        public ICommand SubmitCommand { get; }
+
+        public ICommand CancelCommand { get; }
+    }
+}
